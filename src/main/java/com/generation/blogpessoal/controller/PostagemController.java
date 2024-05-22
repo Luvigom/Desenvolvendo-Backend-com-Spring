624fc32cdea89a45217c896c737fbe02e459d726
Lucas Vinicius <lucas.vinicius@sumup.com>
2024-05-22 14:17:27 -0300

    Atividade 2 - CRUD

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -1,16 +1,26 @@
 package com.generation.blogpessoal.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+
+import jakarta.validation.Valid;
 
 @RestController //anotação que diz para spring que essa é uma controladora de rotas e acesso aos metodos
 @RequestMapping("/postagens") //rota para chegar nessa classe "insomia"
@@ -24,11 +34,56 @@ public class PostagemController {
 	@GetMapping //defini o verbo http que atende esse metodo
 	public ResponseEntity<List<Postagem>> getAll(){
 		
-		//ResponseEntity - Classe do
+		//ResponseEntity - Classe do Spring que trata a saida de dados para o formato HTTP
 		return ResponseEntity.ok(postagemRepository.findAll());
 		//SELECT * FROM tb_postagens
 	}
 	
-}
+	//localhost:8080/postagens/1
+		@GetMapping("/{id}")
+		public ResponseEntity<Postagem> getById(@PathVariable Long id){
+			// findById = SELECT * FROM tb_postagens WHERE id = 1;
+			return postagemRepository.findById(id)
+					.map(resposta -> ResponseEntity.ok(resposta))
+					.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+		}
+		
+		//SELECT * FROM tb_postagens WHERE titulo = "titulo";
+		@GetMapping("/titulo/{titulo}") //localhost:8080/postagens/titulo/Postagem 02
+		public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+			return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+		}
+		
+		//INSERT INTO tb_postagens (titulo, texto, data) VALUES ("Título", "Texto", "2024-12-31 14:05:01");
+		@PostMapping
+		public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
+			return ResponseEntity.status(HttpStatus.CREATED)
+					.body(postagemRepository.save(postagem));
 	
+	}
+		@PutMapping
+		public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) {
+			return postagemRepository.findById(postagem.getId())
+					.map(resposta-> ResponseEntity.status(HttpStatus.OK)
+					.body(postagemRepository.save(postagem)))
+					.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+			
+		}
+		
+		//DELETE FROM tb_postagens Where id = id;
+		
+		@DeleteMapping("/{id}")
+		public void delete(@PathVariable Long id) {
+			Optional<Postagem> postagem = postagemRepository.findById(id);
+			if(postagem.isEmpty())
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+			postagemRepository.deleteById(id);
+		}
+		
+	}
+		
+		//Get - recuperar as informações do banco de dados
+		//Post - Dá o insert no banco de dados 
+		//Push - Update no banco de dados
+		//Delete - Deleta algum registro do banco de dados
 
